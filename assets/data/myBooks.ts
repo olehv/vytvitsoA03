84c97ecfd0ff2cb25d3cf2c770517866d9a429d9
olehv <38218850+olehv@users.noreply.github.com>
2019-07-23 20:34:27 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,9 @@
+import { bookInfo } from "../../app/bookInfo";
+
+export const MYBOOKS: bookInfo [] =
+[
+    {bname: 'Harry Potten and the Goblet of Fire', bauthor: 'J.K. Rowling', bgenre: 'Fantasy', byear: 2000, picture: 'assets/images/harry_potter.jpg' },
+    {bname: 'Farenheit 451', bauthor: 'Ray Bradbury', bgenre: 'Dystopian', byear: 2001, picture: 'assets/images/f451.jpg' },
+    {bname: 'Fifty Shades of Grey', bauthor: 'E. L. James', bgenre: 'Romance', byear: 2011, picture: 'assets/images/fiftyshades.jpg' },
+    {bname: '1984', bauthor: 'George Orwell', bgenre: 'Science Fiction', byear: 1949, picture: 'assets/images/1984.jpg' }
+]
